88ddf596262308bc836ceb11487b79b3a8613b25
Devin Simmons <blackpaw1996@gmail.com>
2014-01-14 20:10:26 -0800

    1.0.3
    Basic Layout Done, Not sure if it works yet can't compile until tomorrow.

--- src/main.c
+++ src/main.c
@@ -1,146 +1,114 @@
 #include "pebble.h"
-//Arrays
-#define NUM_MENU_SECTIONS 3
-#define NUM_MENU_SECTIONS_ALGEBRA 3
-#define NUM_MATH_MENU_ITEMS 4
-#define NUM_SCIENCE_MENU_ITEMS 3
-#define NUM_THIRD_MENU_ITEMS 0
-#define NUM_ALGEBRA_MENU_ITEMS 1
-#define NUM_GEOMETRY_MENU_ITEMS 1
-#define NUM_TRIG_MENU_ITEMS 1
-#define NUM_CALC_MENU_ITEMS 1
-#define NUM_BIOLOGY_MENU_ITEMS 1
-#define NUM_PHYSICS_MENU_ITEMS 1
+#include "second.h"
+#include "algebra.h"
+#include "geometry.h"
+#include "trigonometry.h"
+#include "calc.h"
+#include "biology.h"
+#include "physics.h"
 	
+#define NUM_MATH_MENU_ITEMS 6
+#define NUM_MENU_SECTIONS 1
+
 static Window *window;
-static Window *AlgebraWindow;
-static Layer *layer2;
-
-//Menu Layers
-static SimpleMenuLayer *algebra_menu_layer;
-static SimpleMenuLayer *geometry_menu_layer;
-static SimpleMenuLayer *trig_menu_layer;
-static SimpleMenuLayer *calc_menu_layer;
-static SimpleMenuLayer *biology_menu_layer;
-static SimpleMenuLayer *physics_menu_layer;
-
-//Advanced Menu Items
-static SimpleMenuItem algebra_menu_items[NUM_ALGEBRA_MENU_ITEMS];
-static SimpleMenuItem geometry_menu_items[NUM_GEOMETRY_MENU_ITEMS];
-static SimpleMenuItem trig_menu_items[NUM_TRIG_MENU_ITEMS];
-static SimpleMenuItem calc_menu_items[NUM_CALC_MENU_ITEMS];
-static SimpleMenuItem biology_menu_items[NUM_BIOLOGY_MENU_ITEMS];
-static SimpleMenuItem physics_menu_items[NUM_PHYSICS_MENU_ITEMS];
 
 static SimpleMenuLayer *simple_menu_layer;
 
-// Sections for types of Equations
 static SimpleMenuSection menu_sections[NUM_MENU_SECTIONS];
-//Menu Items for Each Section
+
+//Menu Items
 static SimpleMenuItem math_menu_items[NUM_MATH_MENU_ITEMS];
 
-static SimpleMenuItem science_menu_items[NUM_SCIENCE_MENU_ITEMS];
 
-static SimpleMenuItem third_menu_items[NUM_THIRD_MENU_ITEMS];
 
-void AlgebraWindow_unload(Window *AlgebraWindow) {
-  window_destroy(AlgebraWindow);
+
+//Callbacks
+
+static void menu_select_callback(int index, void *ctx) {
+  show_second_menu();
 }
-//Callbacks for Functionality
 static void algebra_select_callback(int index, void *ctx) {
-  AlgebraWindow = window_create();
-	
-	int num2_a_items = 0;
-  algebra_menu_items[num2_a_items++] = (SimpleMenuItem){
-    .title = "Algebra1",
-        .subtitle = "Basic2 Alegbra Equations Test",
-  };
-
-
-	
-  Layer *window_layer = window_get_root_layer(AlgebraWindow);
-  GRect bounds = layer_get_frame(window_layer);
-  layer2 = layer_create(bounds);
-
-  algebra_menu_layer = simple_menu_layer_create(bounds, window, menu_sections, NUM_MENU_SECTIONS_ALGEBRA, NULL);
-
-  layer_add_child(window_layer, simple_menu_layer_get_layer(algebra_menu_layer));
-
-  window_stack_push(AlgebraWindow, true /* Animated */);
-	app_event_loop();
-	
+  show_algebra_menu();
+}
+static void geometry_select_callback(int index, void *ctx) {
+  show_geometry_menu();
+}
+static void trig_select_callback(int index, void *ctx) {
+  show_trig_menu();
+}
+static void calc_select_callback(int index, void *ctx) {
+  show_calc_menu();
+}
+static void bio_select_callback(int index, void *ctx) {
+  show_bio_menu();
+}
+static void physics_select_callback(int index, void *ctx) {
+  show_physics_menu();
 }
 
 
 static void window_load(Window *window) {
+
   int num_a_items = 0;
+
   math_menu_items[num_a_items++] = (SimpleMenuItem){
     .title = "Algebra",
-        .subtitle = "Basic Alegbra Equations",
-        .callback = algebra_select_callback,
+	.subtitle = "Algebraic Equations",
+    .callback = algebra_select_callback,
   };
   math_menu_items[num_a_items++] = (SimpleMenuItem){
     .title = "Geometry",
-    .subtitle = "Geometric Equations",
+	.subtitle = "Geometric Equations",
+    .callback = geometry_select_callback,
   };
   math_menu_items[num_a_items++] = (SimpleMenuItem){
     .title = "Trigonometry",
-    .subtitle = "Trig Equations",
+	.subtitle = "Trig Equations",
+    .callback = trig_select_callback,
   };
   math_menu_items[num_a_items++] = (SimpleMenuItem){
     .title = "Calculus",
-    .subtitle = "I Love Calc",
+	.subtitle = "Calc Equations",
+    .callback = calc_select_callback,
   };
-        
-  // Science Menu Area
-
-  science_menu_items[0] = (SimpleMenuItem){
-    .title = "Biology",
-    .subtitle = "Biological Equations",
+  math_menu_items[num_a_items++] = (SimpleMenuItem){
+    .title = "Biology Equations",
+	.subtitle = "Bio Equations",
+    .callback = bio_select_callback,
   };
-  science_menu_items[1] = (SimpleMenuItem){
-    .title = "Chemistry",
-    .subtitle = "Chemical Equations",
-  };        
-  science_menu_items[2] = (SimpleMenuItem){
-    .title = "Physics",
-    .subtitle = "Physics Equations",
+  math_menu_items[num_a_items++] = (SimpleMenuItem){
+    .title = "Physics Equations",
+	.subtitle = "Yay Physics!",
+    .callback = physics_select_callback,
   };
-  // Bind the menu items to the corresponding menu sections
   menu_sections[0] = (SimpleMenuSection){
-        .title ="Math Equations",
-    .num_items = NUM_MATH_MENU_ITEMS,
-          .items = math_menu_items,
+  .title ="Categories",
+  .num_items = NUM_MATH_MENU_ITEMS,
+  .items = math_menu_items,
   };
-  menu_sections[1] = (SimpleMenuSection){
-    // Menu sections can also have titles as well
-    .title = "Science Equations",
-    .num_items = NUM_SCIENCE_MENU_ITEMS,
-    .items = science_menu_items,
-  };
-  menu_sections[2] = (SimpleMenuSection){
-                .title = "More To Come",
-                .num_items = NUM_THIRD_MENU_ITEMS,
-                .items = third_menu_items,
-
-        };
-
+	
+  // Now we prepare to initialize the simple menu layer
+  // We need the bounds to specify the simple menu layer's viewport size
+  // In this case, it'll be the same as the window's
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_frame(window_layer);
 
-  simple_menu_layer = simple_menu_layer_create(bounds, window, menu_sections, NUM_MENU_SECTIONS, NULL);
+  // Initialize the simple menu layer
+  simple_menu_layer = simple_menu_layer_create(bounds, window, menu_sections,NUM_MENU_SECTIONS, NULL);
 
+  // Add it to the window for display
   layer_add_child(window_layer, simple_menu_layer_get_layer(simple_menu_layer));
 }
 
+// Deinitialize resources on window unload that were initialized on window load
 void window_unload(Window *window) {
   simple_menu_layer_destroy(simple_menu_layer);
-
 }
 
 int main(void) {
   window = window_create();
 
+  // Setup the window handlers
   window_set_window_handlers(window, (WindowHandlers) {
     .load = window_load,
     .unload = window_unload,
---
+++ src/trigonometry.h
@@ -0,0 +1,12 @@
+#include "pebble.h"
+#include "second.h"
+#include "algebra.h"
+#include "geometry.h"
+#include "trigonometry.h"
+#include "calc.h"
+#include "biology.h"
+#include "physics.h"
+
+void show_trigonometry_menu();
+
+#endif
